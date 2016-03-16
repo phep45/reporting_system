d0075c787674220aab9e4c6f7760ef9a1894227e
Prosner <phep45@gmail.com>
2016-03-16 12:15:30 +0100

    MQListener added SLISListener added XLISListener added

---
+++ src/main/java/listeners/MQListener.java
@@ -0,0 +1,8 @@
+package listeners;
+
+/**
+ * Created by Prosner on 3/16/2016.
+ *
+ */
+public interface MQListener {
+}
---
+++ src/main/java/listeners/SLISListener.java
@@ -0,0 +1,9 @@
+package listeners;
+
+/**
+ * Created by Prosner on 3/16/2016.
+ *
+ */
+public class SLISListener implements MQListener {
+
+}
