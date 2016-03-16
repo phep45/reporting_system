bd3ef4fa1ffc145a24261c420f6890a0c9dd5974
Prosner <phep45@gmail.com>
2016-03-16 17:08:36 +0100

    reworked

--- src/main/java/app/ReportingSystemApp.java
+++ src/main/java/app/ReportingSystemApp.java
@@ -6,15 +6,9 @@ import listeners.MQListenerImpl;
 public class ReportingSystemApp {
 
     public static void main(String[] args) {
-        String url = "tcp://localhost:61616";
-        String SLIS = "SLIS";
-        String XLIS = "XLIS";
-
-        MQListener slisListener = new MQListenerImpl(SLIS);
-        MQListener xlisListener = new MQListenerImpl(XLIS);
+        MQListener slisListener = new MQListenerImpl("SLIS");
 
         slisListener.listen();
-        System.out.println("end");
 
     }
 
