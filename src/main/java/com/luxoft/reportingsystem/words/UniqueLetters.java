d2c496d73a8cc75911dafb2ed9909623ff84448f
Prosner <phep45@gmail.com>
2016-03-18 12:30:09 +0100

    reworked

--- src/main/java/com/luxoft/reportingsystem/words/UniqueLetters.java
+++ src/main/java/com/luxoft/reportingsystem/words/UniqueLetters.java
@@ -11,19 +11,20 @@ public class UniqueLetters {
     private Map<Set<String>, Integer> map;
 
     public UniqueLetters(File file) {
+        if (!file.getName().endsWith(".txt"))
+            throw new IllegalArgumentException();
         map = countUniques(file);
     }
 
     private Map<Set<String>, Integer> countUniques(File file) {
-
         map = new HashMap<>();
 
         try {
             FileInputStream fileInputStream = new FileInputStream(file);
             Scanner scan = new Scanner(fileInputStream);
-            while(scan.hasNext()) {
+            while (scan.hasNext()) {
                 Set<String> set = new TreeSet<>(Arrays.asList(scan.next().split("")));
-                if(!map.containsKey(set))
+                if (!map.containsKey(set))
                     map.put(set, 1);
                 else {
                     Integer val = map.get(set);
@@ -40,7 +41,7 @@ public class UniqueLetters {
     }
 
     public List<Integer> getAmounts() {
-        if(map == null)
+        if (map == null)
             return null;
         List<Integer> listOfAmounts = map.values().stream().collect(Collectors.toList());
         Collections.sort(listOfAmounts, (val1, val2) -> val2.compareTo(val1));
@@ -48,15 +49,14 @@ public class UniqueLetters {
     }
 
     public void printUniques() {
-        if(map != null)
-            for(Set s : map.keySet())
+        if (map != null)
+            for (Set s : map.keySet())
                 System.out.println(map.get(s) + " => " + s);
     }
 
 
     public static void main(String[] args) {
         UniqueLetters uniqueLetters = new UniqueLetters(new File("src\\main\\resources\\words\\INPUT.txt"));
-//        uniqueLetters.countUniques();
         System.out.println(uniqueLetters.getAmounts());
         uniqueLetters.printUniques();
     }
--- src/test/java/com/luxoft/reportingsystem/words/UniqueLettersTest.java
+++ src/test/java/com/luxoft/reportingsystem/words/UniqueLettersTest.java
@@ -3,15 +3,24 @@ package com.luxoft.reportingsystem.words;
 import org.junit.Test;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class UniqueLettersTest {
 
     private UniqueLetters uniqueLetters;
+    private List<Integer> outputForTest = Arrays.asList(3, 2, 2, 1, 1);
 
     @Test
     public void test() {
-        uniqueLetters = new UniqueLetters(new File("src\\main\\resources\\words\\test2.txt"));
-
+        uniqueLetters = new UniqueLetters(new File("src\\main\\resources\\words\\test.txt"));
+        assertEquals(outputForTest, uniqueLetters.getAmounts());
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowException() {
+        uniqueLetters = new UniqueLetters(new File("asdfasfdssde"));
+    }
 }
