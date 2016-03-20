26617b0e05baa198a8aa2cc105ff13a4ee063c38
HAL <phep45@gmail.com>
2016-03-20 15:02:12 +0100

    reworked

--- src/main/java/com/luxoft/wordscounter/WordsConfig.java
+++ src/main/java/com/luxoft/wordscounter/WordsConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.luxoft.wordscounter.words")
+@ComponentScan("com.luxoft.wordscounter")
 public class WordsConfig {
 
 }
