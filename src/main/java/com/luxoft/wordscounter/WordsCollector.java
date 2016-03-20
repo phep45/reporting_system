457eda94cf490a9d45dc3dfeaba81b8b372c403a
HAL <phep45@gmail.com>
2016-03-20 15:50:44 +0100

    reworked

--- src/main/java/com/luxoft/wordscounter/WordsCollector.java
+++ src/main/java/com/luxoft/wordscounter/WordsCollector.java
@@ -3,25 +3,20 @@ package com.luxoft.wordscounter;
 import com.google.common.base.Preconditions;
 import org.springframework.stereotype.Component;
 
-import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class WordsCollector {
 
-    private static final String SPECIAL_CHARACTERS = "[\\W]";
+    private static final String SPECIAL_CHARACTERS = "\\W";
     private static final String EMPTY = "";
 
     public List<String> collect(List<String> listOfWords) {
         Preconditions.checkNotNull(listOfWords);
-        List<String> collected = new LinkedList<>();
-
-        listOfWords.forEach(str -> {
-            str = str.replaceAll(SPECIAL_CHARACTERS, EMPTY);
-            collected.add(str);
-        });
-
-        return collected;
+        return listOfWords.stream()
+                .map(str -> str.replaceAll(SPECIAL_CHARACTERS, EMPTY))
+                .collect(Collectors.toList());
     }
 
 }
