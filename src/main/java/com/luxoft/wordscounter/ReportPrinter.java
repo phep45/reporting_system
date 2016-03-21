85c496b5a7a3177bf3c896e246fa0cc77c59d575
Prosner <phep45@gmail.com>
2016-03-21 10:43:24 +0100

    constant extracted

--- src/main/java/com/luxoft/wordscounter/ReportPrinter.java
+++ src/main/java/com/luxoft/wordscounter/ReportPrinter.java
@@ -7,10 +7,13 @@ import java.util.Map;
 
 @Component
 public class ReportPrinter {
+
+    private static final String SEPARATOR = " => ";
+
     public void printReport(Map<SetOfPairs, Integer> map) {
         Preconditions.checkNotNull(map, "No data to print");
         for (Map.Entry<SetOfPairs, Integer> s : map.entrySet()) {
-            System.out.println(s.getValue() + " => " + s.getKey());
+            System.out.println(s.getValue() + SEPARATOR + s.getKey());
         }
     }
 
