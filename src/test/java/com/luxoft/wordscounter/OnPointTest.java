48c6802864667bb2b71aff7d4d80ce7d6c2fe84e
Prosner <phep45@gmail.com>
2016-03-22 17:25:24 +0100

    in progress

--- src/test/java/com/luxoft/wordscounter/OnPointTest.java
+++ src/test/java/com/luxoft/wordscounter/OnPointTest.java
@@ -2,6 +2,11 @@ package com.luxoft.wordscounter;
 
 import com.google.common.collect.ImmutableMap;
 import org.apache.commons.lang3.tuple.MutablePair;
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.hamcrest.core.AllOf;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -59,6 +64,34 @@ public class OnPointTest {
         verify(wordsCollectorMock).collect(expectedReaderOutput);
         verify(uniqueLettersMock).count(expectedCollectorOutput);
         verify(reportPrinter).printReport(expectedUniqueLetters);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldProcessCorrectlyBaseOnSizes() throws IOException {
+
+        final int expectedSize = expectedReaderOutput.size();
+
+        when(textFileReaderMock.readFromFile(file)).thenReturn(expectedReaderOutput);
+        when(wordsCollectorMock.collect(expectedReaderOutput)).thenReturn(expectedCollectorOutput);
+        when(uniqueLettersMock.count(expectedCollectorOutput)).thenReturn(expectedUniqueLetters);
+
+        onPoint.process(path);
+
+        verify(textFileReaderMock).readFromFile(file);
+        verify(wordsCollectorMock).collect(argThat(AllOf.allOf(Matchers.hasSize(expectedSize))));
+        verify(uniqueLettersMock).count(argThat(AllOf.allOf(Matchers.hasSize(expectedSize))));
+        verify(reportPrinter).printReport(argThat(new BaseMatcher<Map<TreeSet<MutablePair<String, Integer>>, Integer>>() {
+            @Override
+            public void describeTo(Description description) {
+
+            }
+
+            @Override
+            public boolean matches(Object o) {
+                return ((Map<TreeSet<MutablePair<String, Integer>>, Integer>) o).size() == 2;
+            }
+        }));
     }
 
     private static ImmutableMap<TreeSet<MutablePair<String, Integer>>, Integer> generateExpectedUniqueLetters() {
