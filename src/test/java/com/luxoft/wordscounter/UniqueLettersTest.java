5e95dde461322e79ee75ab1f0d528ddd6fc4c1e8
Prosner <phep45@gmail.com>
2016-03-23 10:42:07 +0100

    unused import removed

--- src/test/java/com/luxoft/wordscounter/UniqueLettersTest.java
+++ src/test/java/com/luxoft/wordscounter/UniqueLettersTest.java
@@ -1,19 +1,11 @@
 package com.luxoft.wordscounter;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSortedMap;
 import org.apache.commons.lang3.tuple.MutablePair;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 
