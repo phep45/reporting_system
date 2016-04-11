d9875da63acc92be86697e7eeffe6cd9f59c990c
HAL <phep45@gmail.com>
2016-04-11 23:31:58 +0200

    transactional management

--- rs_db/src/main/java/com/luxoft/jmswithspring/database/dao/SuperDAO.java
+++ rs_db/src/main/java/com/luxoft/jmswithspring/database/dao/SuperDAO.java
@@ -4,9 +4,14 @@ import com.luxoft.jmswithspring.model.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 @Component
 public class SuperDAO {
@@ -15,6 +20,8 @@ public class SuperDAO {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private PlatformTransactionManager txManager;
 
     @Autowired
     private BaseDAO<User> userDAO;
@@ -27,21 +34,41 @@ public class SuperDAO {
     @Autowired
     private BaseDAO<SecuritiesForBranches> securitiesForBranchesDAO;
 
-    @Transactional
+    //    @Transactional
     public void safelyInsert(Transaction transaction) {
-        userDAO.safelyInsert(transaction.getUser());
-        branchDAO.safelyInsert(transaction.getBranch());
-        
-        transactionDAO.safelyInsert(transaction);
-        transaction.getLots().getListOfLots().forEach(lot -> {
-            lotDAO.safelyInsertSecurity(lot);
-            lotDAO.safelyInsert(lot, transaction.getId());
-        });
+        TransactionDefinition definition = new DefaultTransactionDefinition();
+        TransactionStatus status = txManager.getTransaction(definition);
+
+        try {
+            userDAO.safelyInsert(transaction.getUser());
+            branchDAO.safelyInsert(transaction.getBranch());
+
+            transactionDAO.safelyInsert(transaction);
+            transaction.getLots().getListOfLots().forEach(lot -> {
+                lotDAO.safelyInsertSecurity(lot);
+                lotDAO.safelyInsert(lot, transaction.getId());
+            });
+            txManager.commit(status);
+        } catch (DataAccessException e) {
+            txManager.rollback(status);
+            log.error("Transaction failed. Rollback.", e);
+            return;
+        }
         log.info("Whole transaction added to database.");
     }
 
     public void safelyInsert(SecuritiesForBranches securitiesForBranches) {
-        securitiesForBranchesDAO.safelyInsert(securitiesForBranches);
+        TransactionDefinition definition = new DefaultTransactionDefinition();
+        TransactionStatus status = txManager.getTransaction(definition);
+
+        try {
+            securitiesForBranchesDAO.safelyInsert(securitiesForBranches);
+            txManager.commit(status);
+        } catch(DataAccessException e) {
+            txManager.rollback(status);
+            log.error("Transaction failed. Rollback.", e);
+            return;
+        }
         log.info("All securities for branch added to database.");
     }
 
