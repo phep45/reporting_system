8531ea18146d5737e658e80b613980120763ffdf
HAL <phep45@gmail.com>
2016-04-21 23:01:53 +0200

    internal unique ID - in progress

---
+++ rs_app/src/test/java/com/luxoft/jmswithspring/camel/internalid/InternalIdTestConfig.java
@@ -0,0 +1,10 @@
+package com.luxoft.jmswithspring.camel.internalid;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.luxoft.jmswithspring.camel.*")
+public class InternalIdTestConfig {
+
+}
