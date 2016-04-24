1cdd0ae76bb1e29fbf7adc9f1fe2932d56845388
HAL <phep45@gmail.com>
2016-04-24 23:42:09 +0200

    uniqueId changed type to AtomicInteger

--- rs_app/src/main/java/com/luxoft/jmswithspring/camel/internalid/InternalId.java
+++ rs_app/src/main/java/com/luxoft/jmswithspring/camel/internalid/InternalId.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 //@Component
 public class InternalId {
@@ -15,21 +16,21 @@ public class InternalId {
     private static final int INTERSPACE = 100;
 
     private final File internalIdFile;
-    private int uniqueId;
+    private AtomicInteger uniqueId;
 
 
     public InternalId() throws IOException {
         this.internalIdFile = DEFAULT_INTERNAL_ID_FILE;
-        uniqueId = Integer.parseInt(FileUtils.readFileToString(internalIdFile));
+        uniqueId = new AtomicInteger(Integer.parseInt(FileUtils.readFileToString(internalIdFile)));
     }
 
     public InternalId(File internalIdFile) throws IOException {
         this.internalIdFile = internalIdFile;
-        uniqueId = Integer.parseInt(FileUtils.readFileToString(internalIdFile));
+        uniqueId = new AtomicInteger(Integer.parseInt(FileUtils.readFileToString(internalIdFile)));
     }
 
     public void increment() {
-        uniqueId++;
+        uniqueId.getAndIncrement();
         log.info("Internal ID incremented to {}", uniqueId);
         if(interspaceReached()) {
             updateInternalIdFile();
@@ -47,10 +48,10 @@ public class InternalId {
     }
 
     private boolean interspaceReached() {
-        return uniqueId % INTERSPACE == 0;
+        return uniqueId.get() % INTERSPACE == 0;
     }
 
     public int getUniqueId() {
-        return uniqueId;
+        return uniqueId.get();
     }
 }
