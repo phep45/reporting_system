b531634d87656aeb7532d07c0732ebea862d6b32
HAL <phep45@gmail.com>
2016-05-04 10:08:49 +0200

    slis validation

--- rs_service/src/main/java/com/luxoft/jmswithspring/validator/SlisConsistencyValidator.java
+++ rs_service/src/main/java/com/luxoft/jmswithspring/validator/SlisConsistencyValidator.java
@@ -3,6 +3,9 @@ package com.luxoft.jmswithspring.validator;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Component
 @Qualifier("slisValidator")
 public class SlisConsistencyValidator implements Validator<String> {
@@ -10,8 +13,8 @@ public class SlisConsistencyValidator implements Validator<String> {
 
     @Override
     public boolean validate(String slis) {
-
-
-        return false;
+        Pattern pattern = Pattern.compile(VALID_SLIS);
+        Matcher matcher = pattern.matcher(slis);
+        return matcher.find();
     }
 }
